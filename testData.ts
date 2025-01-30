4d91a193057b46c87db083981444b0e14d98aef6
AgentXUN <xunagentxun@gmail.com>
2025-01-30 18:15:59 +0100

    Add files via upload

---
+++ testData.ts
@@ -0,0 +1,20 @@
+export const TEST_MENTIONS = {
+  data: [
+    {
+      id: '1234567890',
+      text: '@AgentXun Hey! I have an idea for a token based on renewable energy credits. Each token represents 1 MWh of clean energy.',
+    },
+    {
+      id: '1234567891',
+      text: '@AgentXun What do you think about the market today?',
+    },
+    {
+      id: '1234567892',
+      text: '@AgentXun Launch a token for decentralized cloud storage! Users stake tokens to provide storage space.',
+    },
+    {
+      id: '1234567893',
+      text: '@AgentXun wen moon ser?',
+    }
+  ]
+}; 
